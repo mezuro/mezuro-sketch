1111ff9b60cd36b75efd15f31a391ef081e9fb1c
joaomm <joaomm88@gmail.com>
2010-03-19 16:17:10 -0300

    Implementing the Project create action (Vivi, Ana and Joao)

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :repository_url
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,15 +1,33 @@
 require 'spec_helper'
 
 describe Project do
-  before(:each) do
-    @valid_attributes = {
+
+  def valid_attributes(attributes={})
+    {
       :name => "value for name",
       :description => "value for description",
       :repository_url => "value for repository_url"
-    }
+    }.merge attributes
   end
 
   it "should create a new instance given valid attributes" do
-    Project.create!(@valid_attributes)
+    Project.create!(valid_attributes)
+  end
+
+  context "creating instances with invalid attributes" do
+    it "should not create a new instance given invalid name" do
+      project = Project.new(valid_attributes(:name => nil))
+      project.save.should == false
+    end
+
+    it "should not create a new instance given invalid description" do
+      project = Project.new(valid_attributes(:description => nil))
+      project.save.should == false
+    end
+
+    it "should not create a new instance given invalid url" do
+      project = Project.new(valid_attributes(:repository_url => nil))
+      project.save.should == false
+    end
   end
 end
