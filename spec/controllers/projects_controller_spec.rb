30d67891ab052bd14edc147d466c7a6c1f4ba37e
Thiago Colucci <ticolucci@gmail.com>
2010-03-19 14:47:44 -0300

    Merging project(s)_controller (Thiago)

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -21,4 +21,23 @@ describe ProjectsController do
     end
 
   end
+  
+  describe "POST create" do
+    it "should create a project given valid attributes" do
+      post :create, :project => {:name => 'Projeto Mezuro',
+        :description => 'Projeto para visualização de métricas',
+        :repository_url => 'git://github.com/paulormm/mezuro.git'}
+      Project.find_by_name("Projeto Mezuro").should_not be_nil
+    end
+
+    it "should not create a project given nil attributes" do
+      post :create, :project => {:name => nil,
+        :description => nil,
+        :repository_url => nil}
+      Project.find_by_name(nil).should be_nil
+    end
+
+
+  end
+
 end
