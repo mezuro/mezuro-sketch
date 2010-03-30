8c984f2dd8c1f55e02590da0d72620752a978e61
viviane a. dos santos <viviane.almeida@gmail.com>
2010-03-30 16:20:52 -0300

    "Creating user model and view (Viviane & Lucianna)"

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,57 @@ require 'spec_helper'
 
 describe User do
 
-  it "should create an instance given valid attributes" do
-    User.create(:login => "pika", :password => "gordo", :email => "pika@agilbits.com")
+  def valid_attributes(attributes={})
+    {
+      :login => "user_login",
+      :password => "user_password",
+      :email => "name@place.com",
+    }.merge attributes
   end
+
+  it "should create an instance given valid attributes" do
+    User.create!(valid_attributes)
+  end
+
+  context "creating user with invalid attributes" do
+    it "should not create an instance given invalid login" do
+      user = User.new(valid_attributes(:login => nil))
+      user.save.should == false
+
+      user = User.new(valid_attributes(:login => ""))
+      user.save.should == false
+    end
+
+    it "should not create an instance given invalid password" do
+      user = User.new(valid_attributes(:password => nil))
+      user.save.should == false
+
+      user = User.new(valid_attributes(:password => ""))
+      user.save.should == false
+    end
+
+    it "should not create an instance given invalid e-mail" do
+      user = User.new(valid_attributes(:email => nil))
+      user.save.should == false
+
+      user = User.new(valid_attributes(:email => ""))
+      user.save.should == false
+    end     
+  end
+
+  context "validating e-mail" do
+    it "should not create an instance when e-mail is without @" do
+      user = User.new(valid_attributes(:email => "name_at_place.com"))
+      user.save.should == false      
+    end
+  end
+
+  context "validating login" do
+    it "should only create an instance given an unique login" do
+      User.create!(valid_attributes(:login => "pika"))
+      user = User.new(valid_attributes(:login => "pika"))
+      user.save.should == false
+    end
+  end
+
 end
