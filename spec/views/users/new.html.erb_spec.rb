fe85dd908d8c37521cd0a50d31484034d1963871
viviane a. dos santos <viviane.almeida@gmail.com>
2010-03-30 16:32:41 -0300

    creating users/new view (Viviane & Lucianna)

---
+++ spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe "/users/new" do
+  context "without errors" do
+    before :each do
+      assigns[:user] = User.new
+      render
+    end
+
+    it "should have a title: New User" do
+      response.should have_tag("h1", "New User")
+    end
+
+    it "should have a form" do
+      response.should have_tag("form") do
+        with_tag("label", "User Login")
+        with_tag("input[id=?][type=?]", "user_login", "text")
+        with_tag("label", "Password")
+        with_tag("input[id=?][type=?]", "user_password", "text")
+        with_tag("label", "e-mail")
+        with_tag("input[id=?][type=?]", "user_email", "text")
+        with_tag("input[id=?][type=?]", "user_submit", "submit")
+      end
+    end
+
+  end
+end
+
