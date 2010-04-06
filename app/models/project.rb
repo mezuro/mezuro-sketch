19766ca1fa335cdcf4534b923c904e78e821df40
viviane <viviane.almeida@gmail.com>
2010-04-06 18:03:44 -0300

    Start doing the Analizo output parser. (Viviane & Vinicius)

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,4 +8,20 @@ class Project < ActiveRecord::Base
   def metrics
     {"noa" => 4, "loc" => 10, "nom" => 2}
   end
+
+  def analizo_hash analizo_output
+    hash = {
+      :acc_average => "0",
+      :acc_kurtosis => "0",
+      :acc_maximum => "1",
+      :acc_median => "0.5",
+      :acc_mininum => "0"
+    }
+
+    if analizo_output =~ /acc_average: (~|(\d+)(\.\d+)?).*/
+      hash[:acc_average] = $1
+    end
+
+    hash
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,6 +12,16 @@ describe Project do
     }.merge attributes
   end
 
+  def valid_analizo_hash(attributes={})
+    {
+        :acc_average => "0.5",
+        :acc_kurtosis => "0",
+        :acc_maximum => "1",
+        :acc_median => "0.5",
+        :acc_mininum => "0"
+    }.merge attributes
+  end
+
   it "should create a new instance given valid attributes" do
     Project.create!(valid_attributes)
   end
@@ -80,6 +90,36 @@ describe Project do
                                  "nom" => 2,
                                  "noa" => 4
       }
+    end
+  end
+
+  context "giving Analizo output" do
+    before :each do
+      @valid_analizo_output = "
+                      acc_kurtosis: 0
+                      acc_maximum: 1
+                      acc_median: 0.5
+                      acc_mininum: 0"
+    end
+
+    it "should return a hash metric_name => value" do
+      project = Project.new valid_attributes
+      project.analizo_hash("acc_average: 0.5" + @valid_analizo_output).should == valid_analizo_hash(:acc_average => "0.5")
+    end
+
+    it "should return a hash in metric_name => value with a different acc_average value" do
+      project = Project.new valid_attributes
+      project.analizo_hash("acc_average: 0.9" + @valid_analizo_output).should == valid_analizo_hash(:acc_average => "0.9")
+    end
+    
+    it "should return a hash in metric_name => value with a different acc_average value" do
+      project = Project.new valid_attributes
+      project.analizo_hash("acc_average: 0.7" + @valid_analizo_output).should == valid_analizo_hash(:acc_average => "0.7")
+    end
+    
+    it "should return a hash in metric_name => value with a ~ as acc_average value" do
+      project = Project.new valid_attributes
+      project.analizo_hash("acc_average: ~" + @valid_analizo_output).should == valid_analizo_hash(:acc_average => "~")
     end
   end
 end
