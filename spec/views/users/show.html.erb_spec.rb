0e99927ba3d1efb089a648ef4359b2075b419f48
marcio rocha dos santos <marcio.santos@salsicha.cec.ime.usp.br>
2010-05-28 16:44:34 -0300

    Show link to new project when user doesnt have projects (Lucianna & Marcio)

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,46 +1,70 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/users/show" do
-  fixtures :projects
-  before :each do
-    assigns[:user] = User.new(:login => "pika",
-      :password => "gordo",
-      :password_confirmation => "gordo",
-      :email => "pika@agilbits.com")
-    assigns[:projects] = [projects(:my_project), projects(:analizo)]
-    render
+
+  context "user has projects" do
+    fixtures :projects
+    before :each do
+      assigns[:user] = User.new(:login => "pika",
+        :password => "gordo",
+        :password_confirmation => "gordo",
+        :email => "pika@agilbits.com")
+      assigns[:projects] = [projects(:my_project), projects(:analizo)]
+      render
+    end
+
+    it "should have a title: User Info" do
+      response.should have_tag("h1", "User Info")
+    end
+
+    it "should have a table with user info" do
+      response.should have_tag("table") do
+        with_tag("tr[id=?]", "tr_user_login") do
+          with_tag("td", "Login")
+          with_tag("td", "pika")
+        end
+      end
+      with_tag("tr[id=?]", "tr_user_email") do
+        with_tag("td", "Email")
+        with_tag("td", "pika@agilbits.com")
+      end   
+    end
+
+    it "should have a title for user projects list" do
+      response.should have_tag("h3", "List of User Projects")
+    end
+
+    it "should have a list of user projects" do
+      response.should have_tag("ul") do
+        with_tag("li") do
+          with_tag("a", projects(:my_project).name, project_path(projects(:my_project).identifier))
+        end
+        with_tag("li") do
+          with_tag("a", projects(:analizo).name, project_path(projects(:analizo).identifier))
+        end
+      end
+    end 
   end
 
-  it "should have a title: User Info" do
-    response.should have_tag("h1", "User Info")
-  end
+  context "user doesn't have projects" do
+    before :each do
+      assigns[:user] = User.new(:login => "vinicius",
+        :password => "magro",
+        :password_confirmation => "magro",
+        :email => "vinicius@qualquercoisa.com")
+      assigns[:projects] = []
+      render
+    end
 
-  it "should have a table with user info" do
-    response.should have_tag("table") do
-      with_tag("tr[id=?]", "tr_user_login") do
-        with_tag("td", "Login")
-        with_tag("td", "pika")
+    it "should not have a title for user projects list" do
+      response.should_not have_tag("h3", "List of User Projects")
+    end
+
+    it "should have a link to new project" do 
+      response.should have_tag("h3") do
+        with_tag("a[href=?]", new_project_path)
       end
     end
-    with_tag("tr[id=?]", "tr_user_email") do
-      with_tag("td", "Email")
-      with_tag("td", "pika@agilbits.com")
-    end   
+
   end
-
-  it "should have a title for user projects list" do
-    response.should have_tag("h3", "List of User Projects")
-  end
-
-  it "should have a list of user projects" do
-    response.should have_tag("ul") do
-      with_tag("li") do
-        with_tag("a", projects(:my_project).name, project_path(projects(:my_project).identifier))
-      end
-      with_tag("li") do
-        with_tag("a", projects(:analizo).name, project_path(projects(:analizo).identifier))
-      end
-    end
-  end 
-
 end
