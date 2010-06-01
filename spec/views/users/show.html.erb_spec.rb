bee0b39f054ee73db0eb9c404ebc30bbeb123487
viviane a. dos santos <viviane.almeida@gmail.com>
2010-06-01 14:57:16 -0300

    Adding E-mail, Created At on the form (Vivi & Lu)

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,42 +1,35 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/users/show" do
-  fixtures :projects, :users
-  before :each do
-    assigns[:user] = users(:viviane)
-    assigns[:projects] = [projects(:my_project), projects(:analizo)]
-    render
-  end
 
   context "user has projects" do
-    fixtures :projects
+    fixtures :projects, :users
     before :each do
-      assigns[:user] = User.new(:login => "pika",
-        :password => "gordo",
-        :password_confirmation => "gordo",
-        :email => "pika@agilbits.com")
+      assigns[:user] = users(:viviane)
       assigns[:projects] = [projects(:my_project), projects(:analizo)]
       render
     end
   
-  it "should have a title: User Info" do
-    response.should have_tag("h1", "User Info")
-  end
-
-  it "should have a table with user info" do
-    response.should have_tag("table") do
-      with_tag("tr[id=?]", "tr_user_login") do
-        with_tag("td", "Login")
-        with_tag("td", "viviane")
-      with_tag("tr[id=?]", "tr_user_email") do
-        with_tag("td", "Email")
-        with_tag("td", "vivi@qualquercoisa.com")
-      end   
-      with_tag("tr[id=?]", "tr_user_creation_date") do
-        with_tag("td", "Created at")
-        with_tag("td", "28 May 2010")
+    it "should have a title: User Info" do
+      response.should have_tag("h1", "User Info")
     end
-  end
+
+    it "should have a table with user info" do
+      response.should have_tag("table") do
+        with_tag("tr[id=?]", "tr_user_login") do
+          with_tag("td", "Login")
+          with_tag("td", "viviane")
+        end
+        with_tag("tr[id=?]", "tr_user_email") do
+          with_tag("td", "Email")
+          with_tag("td", "vivi@qualquercoisa.com")
+        end   
+        with_tag("tr[id=?]", "tr_user_creation_date") do
+          with_tag("td", "Created at")
+          with_tag("td", "28 May 2010")
+        end
+      end
+    end
 
     it "should have a title for user projects list" do
       response.should have_tag("h3", "List of User Projects")
@@ -59,7 +52,8 @@ describe "/users/show" do
       assigns[:user] = User.new(:login => "vinicius",
         :password => "magro",
         :password_confirmation => "magro",
-        :email => "vinicius@qualquercoisa.com")
+        :email => "vinicius@qualquercoisa.com",
+        :created_at => "28 May 2010")
       assigns[:projects] = []
       render
     end
@@ -73,6 +67,5 @@ describe "/users/show" do
         with_tag("a[href=?]", new_project_path)
       end
     end
-
   end
-end
+ end
