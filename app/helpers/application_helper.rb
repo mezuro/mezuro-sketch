1fdcc76b1b266aa363262eae5a2ed29e5e8f39f0
vkdaros <vinicius.k.daros@gmail.com>
2010-06-08 18:16:28 -0300

    Statistics metrics are colpsed and there is a +/- button (Lucianna & Vinicius).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+ def module_metrics_names
+    [:acc, :accm, :amloc, :anpm, :cbo, :dit, :lcom4, :mmloc, :noc, :nom, :npm, :npv, :rfc, :tloc]
+  end
+
+  def statistical_value
+    [:kurtosis, :maximum, :median, :minimum, :mode, :skewness, :standard_deviation, :sum, :variance]
+  end
+  
+  def value_of preffix, suffix, metric_list
+    searched = preffix.to_s + '_' + suffix.to_s
+    metric_list.each do |metric|  
+      return metric.value if metric.name == searched
+    end
+  end
+
 end
